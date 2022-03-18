3d71f3c8dc18b2312e41acd6c6c30e417a8ff2e3
ggorecki <gorecki.gracjan@gmail.com>
2022-03-18 17:34:04 +0100

    theme added

--- install_programs.sh
+++ install_programs.sh
@@ -4,16 +4,14 @@ install(){
   done < $1 
 }
 
+# pass arg from command line
 gui=$1
-
 
 install ./main/list.txt
 
 if [ ! -z "$gui" ]; then
   install $gui 
+  sudo systemctl enable gdm
 fi
-
-
-
 
 sudo ln -s /usr/bin/nvim /usr/bin/vim
--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ makepkg -si
 cd ..
 
 paru -S --noconfirm fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
 bash ./install_programs.sh
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
