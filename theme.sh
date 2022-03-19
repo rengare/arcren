6277e55a92b08e3efa20d79e5b440a0691e67c42
ggorecki <gorecki.gracjan@gmail.com>
2022-03-19 10:17:00 +0100

    theme added

---
+++ theme.sh
@@ -0,0 +1,11 @@
+mkdir ~/.themes
+mkdir ~/.icons
+
+sudo cp -rf /usr/share/themes/Gruvbox-Material-Dark* ~/.themes/
+sudo cp -rf /usr/share/icons/Gruvbox-Material-Dark ~/.icons/
+
+sudo flatpak override --filesystem=$HOME/.themes
+sudo flatpak override --filesystem=$HOME/.icons
+sudo flatpak override --env=GTK_THEME=Gruvbox-Material-Dark
+
+
