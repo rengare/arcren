59db4c7573cb81d129bfa119cb15130caef880a1
ggorecki <gorecki.gracjan@gmail.com>
2022-03-21 13:01:50 +0100

    postinstall for de

--- 2-postinstall.sh
+++ 2-postinstall.sh
@@ -3,3 +3,11 @@ paru -S wireplumber
 systemctl --user enable --now pipewire pipewire-pulse wireplumber
 sudo systemctl enable --now bluetooth 
 sudo systemctl enable --now tlp 
+
+gui=$1
+
+# check if gui is set
+if [ -z "$gui" ]; then
+    bash $gui/postinstall.sh
+fi
+
