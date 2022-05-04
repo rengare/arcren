b6c38709af5f019c59435b7fd39888a85c71617f
Gracjan Górecki <gorecki.gracjan@gmail.com>
2022-05-04 17:50:16 +0200

    Update 1-start.sh

--- 1-start.sh
+++ 1-start.sh
@@ -9,8 +9,8 @@ bash ./scripts/install_main.sh
 
 gui=$1
 extra=$2
-bash .scripts/install_de.sh $gui
-bash .scripts/install_extra.sh $extra
-bash .scripts/theme.sh $gui
+bash scripts/install_de.sh $gui
+bash scripts/install_extra.sh $extra
+bash scripts/theme.sh $gui
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
