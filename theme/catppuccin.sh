d20c5625fd623a00a131a694decf61b255905358
ggorecki <gorecki.gracjan@gmail.com>
2022-12-08 12:36:15 +0100

    fonts added

--- theme/catppuccin.sh
+++ theme/catppuccin.sh
@@ -1,11 +1,10 @@
-paru -S nerd-fonts-jetbrains-mono
-
-mkdir ~/.local/share/themes
-mkdir ~/.local/share/fonts
-mkdir ~/.local/share/icons
+mkdir ~/.local/share/themes 2>/dev/null
+mkdir ~/.local/share/fonts 2>/dev/null
+mkdir ~/.local/share/icons 2>/dev/null
 
 cp -rf ./catppuccin/* ~/.local/share/themes/
 cp -rf ./yaru/* ~/.local/share/icons/
+cp -rf ./jetbrains/* ~/.local/share/fonts/
 
 fc-cache ~/.local/share/fonts
 
