4024a541dba4bac03ebb7d9a594d5f98e43a2df3
ggorecki <gracjan.gorecki@dlabs.a>
2023-10-12 11:22:08 +0200

    changes

--- i3/postinstall.sh
+++ i3/postinstall.sh
@@ -1,2 +1,4 @@
 sudo usermod -aG video $USER
-sudo systemctl enable --now lightdm
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+
