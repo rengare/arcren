61198cb9cb285fb46387d8570811ed2c080dc3de
ggorecki <gorecki.gracjan@gmail.com>
2024-08-16 12:38:57 +0200

    changes

--- theme/catppuccin.sh
+++ theme/catppuccin.sh
@@ -4,8 +4,8 @@ mkdir ~/.local/share/fonts 2>/dev/null
 mkdir ~/.local/share/icons 2>/dev/null
 # theme=Yaru-Lavender-dark
 
-cp -rf ./catppuccin/* $HOME/.themes/
-cp -rf ./yaru/* ~/.local/share/icons/
+# cp -rf ./catppuccin/* $HOME/.themes/
+# cp -rf ./yaru/* ~/.local/share/icons/
 cp -rf ./jetbrains/* ~/.local/share/fonts/
 
 fc-cache ~/.local/share/fonts
