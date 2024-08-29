5bf9920d29c82664f47e0f77d009936e95bff5bc
ggorecki <gorecki.gracjan@gmail.com>
2024-08-29 22:41:23 +0200

    changes

--- 1-start.sh
+++ 1-start.sh
@@ -11,6 +11,3 @@ gui=$1
 extra=$2
 bash ./scripts/install_de.sh $gui
 bash ./scripts/install_extra.sh $extra
-
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
---
+++ brew.sh
@@ -0,0 +1,1 @@
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
