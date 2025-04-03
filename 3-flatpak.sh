8a3eb4c1c892383119511db8b4ccc82e98aa6012
ggorecki <gorecki.gracjan@gmail.com>
2025-04-03 10:42:59 +0000

    changes

--- 3-flatpak.sh
+++ 3-flatpak.sh
@@ -1,7 +1,6 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo --user
 
-while read LINE; do 
-    flatpak install flathub $LINE --user -y
-done < ./flatpak/list.txt 
-
+# while read LINE; do
+#     flatpak install flathub $LINE --user -y
+# done < ./flatpak/list.txt
