c85b32bf33f368327d797397adcdac0391de57b2
rengare <gracjan.gorecki@proton.me>
2025-07-13 19:40:55 +0200

    changes

--- hyprland.sh
+++ hyprland.sh
@@ -1,13 +1,6 @@
 # Use dark mode for QT apps too (like kdenlive)
 # sudo pacman -S --noconfirm kvantum-qt5
 paru -S --noconfirm --needed \
-  wget curl unzip inetutils \
-  fd eza fzf ripgrep zoxide bat \
-  wl-clipboard fastfetch btop \
-  man tldr less whois plocate \
-  alacritty bash-completion impala
-
-paru -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprpolkitagent hyprland-qtutils \
   wofi waybar mako swaybg \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
